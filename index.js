7d19bde92a1b734d0b8d0226b0c77b9b02c69dfe
Samuel Joset <samuel@trusk.com>
2020-03-22 13:18:47 +0100

    feat: add sumOf

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const sumOf = _ => _;
+const R = require("ramda");
+
+/** Returns the sum of the values of the same property off all objects in the supplied list
+ *
+ * @sig [a] -> [a] -> Number
+ * @param {String[]} path - the path to work with
+ * @param {Object[]} objects - objects to retrieve values from
+ * @return {Number} sum of the selected properties
+ * @api public
+ * @example
+ *
+ *   sumOf(["a"], [{ a: 2 }, { a: 3, b: 1 }]); //=> 5
+ *
+ */
+const sumOf = R.curry((path, objects) =>
+  R.pipe(
+    R.pluck(path),
+    R.filter(R.is(Number)),
+    R.sum
+  )(array)
+);
 
 module.exports = sumOf;
